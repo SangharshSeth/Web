5c30fbfe360786f9717d07e86316c1fa0e834792
SangharshSeth <sethsangharsh@outlook.com>
2024-03-25 00:33:13 +0000

    update

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
 
-// window.onload = function () {
-//     // document.getElementById('submit').addEventListener('click', function () {
-//         const message = document.querySelector('#plaintext').value;
-//         let encrypted = " ";
-//         for (let i = 0; i < message.length; i++) {
-//             encrypted += String.fromCharCode((message.charCodeAt(i) + 7 - 97) % 26 + 97);
-//         }
-//         console.log(encrypted)
-//         document.getElementById('enc').textContent = encrypted;
-
-//     });
-    
-// }
-
